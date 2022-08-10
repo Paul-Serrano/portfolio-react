a0ac7dde5c3d32ce0d3d7c31690174474364b9a3
Serrano Paul <paul.serrano08374@gmail.com>
2022-08-10 17:44:47 +0200

    coding skill content

--- src/data/codingLangList.js
+++ src/data/codingLangList.js
@@ -18,21 +18,21 @@ const codingLangList = [
         skills: [
             {
                 key: 1,
-                skill: "flexbox"
+                skill: "Flexbox"
             },
             {
                 key: 2,
-                skill: "grid"
+                skill: "Grid"
             },
             {
                 key: 3,
-                skill: "animations"
+                skill: "Animations"
             },
             {
                 key: 4,
-                skill: "responsive design"
+                skill: "Media queries"
             }
-            ]
+        ]
     },
     {
         key: 2,
@@ -42,7 +42,15 @@ const codingLangList = [
         skills: [
             {
                 key: 1,
-                skill: "balises"
+                skill: "Balises"
+            },
+            {
+                key: 2,
+                skill: "Links"
+            },
+            {
+                key: 3,
+                skill: "iframe"
             }
         ]
     },
@@ -51,42 +59,140 @@ const codingLangList = [
         name: "Javascript",
         src: jsIcon,
         alt: "Icône Javascript",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "Maîtrise du DOM"
+            },
+            {
+                key: 2,
+                skill: "Objects / array"
+            },
+            {
+                key: 3,
+                skill: "Méthode map"
+            },
+            {
+                key: 4,
+                skill: "if - else / switch"
+            },
+            {
+                key: 5,
+                skill: "Boucles for / while"
+            }
+        ]
     },
     {
         key: 4,
         name: "PHP",
         src: phpIcon,
         alt: "Icône PHP",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "Gestion formulaires"
+            },
+            {
+                key: 2,
+                skill: "Intéractions BDD"
+            },
+            {
+                key: 3,
+                skill: "if - else / switch"
+            },
+            {
+                key: 4,
+                skill: "Boucles for / while"
+            }
+        ]
     },
     {
         key: 5,
         name: "mySql",
         src: mySqlIcon,
         alt: "Icône mySql",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "CRUD"
+            },
+            {
+                key: 2,
+                skill: "Opérateurs logiques"
+            },
+            {
+                key: 3,
+                skill: "Lier tables (foreign key)"
+            }
+        ]
     },
     {
         key: 6,
         name: "React",
         src: react,
         alt: "Icône React",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "JSX"
+            },
+            {
+                key: 2,
+                skill: "Composants React"
+            },
+            {
+                key: 3,
+                skill: "Passer propriétés (props)"
+            },
+            {
+                key: 4,
+                skill: "useState / useEffect"
+            }
+        ]
     },
     {
         key: 7,
         name: "Java",
         src: java,
         alt: "Icône Java",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "if - else / switch"
+            },
+            {
+                key: 2,
+                skill: "Boucles for / while"
+            },
+            {
+                key: 3,
+                skill: "Classes / Métohdes"
+            },
+            {
+                key: 4,
+                skill: "Constructeurs"
+            },
+            {
+                key: 5,
+                skill: "Polymorphisme"
+            }
+        ]
     },
     {
         key: 8,
         name: "jQuery",
         src: jquery,
         alt: "Icône jQuery",
-        skills: []
+        skills: [
+            {
+                key: 1,
+                skill: "Animations"
+            },
+            {
+                key: 2,
+                skill: "Gestion du DOM"
+            },
+        ]
     }
 ]
 
